940e5d9e29a195839ca666c4ccc50fdba499e8c1
archana <sawan22july@gmail.com>
2018-02-23 19:44:50 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
-
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UtilityDetailComponent } from './utility-detail/utility-detail.component';
+import { UtilityService } from './utility.service';
+import { RouterModule } from '@angular/router';
+import { routes } from './app.router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    UtilityDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [UtilityService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/utility-detail/utility-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-utility-detail',
+  templateUrl: './utility-detail.component.html',
+  styleUrls: ['./utility-detail.component.css']
+})
+export class UtilityDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
